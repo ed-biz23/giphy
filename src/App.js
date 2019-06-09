fd67230a35e134a182f6fe44c86ac6cb860d782a
Edward Biswas <edbiswas@gmail.com>
2019-06-09 12:37:28 -0400

    Finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "reactstrap";
+import { v4 } from "uuid";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+import SearchField from "./components/SearchField";
+import GifView from "./components/views/GifView";
+
+const App = () => {
+  const API_KEY = "1hOSiwyFAu6RUOiLerPpGfXzCVqILCrG";
+  const [gifs, setGifs] = useState([]);
+  const [url, setUrl] = useState(
+    `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}`
+  );
+  const [viewing, setViewing] = useState("Trending");
+  const [isRandom, setIsRandom] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const resp = await axios(url);
+        if (isRandom) {
+          setGifs([resp.data.data]);
+          setIsRandom(false);
+        } else {
+          setGifs(resp.data.data);
+        }
+      } catch (error) {
+        alert(error);
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  const search = value => {
+    setUrl(`http://api.giphy.com/v1/gifs/search?q=${value}&api_key=${API_KEY}`);
+    setViewing(value);
+  };
+
+  const random = () => {
+    setIsRandom(true);
+    setUrl(`http://api.giphy.com/v1/gifs/random?&api_key=${API_KEY}#${v4()}`);
+    setViewing("Random");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchField search={search} random={random} />
+      <Container>
+        <h2>Currently Viewing: {viewing} GIFS</h2>
+        <br />
+        <Row>
+          {gifs.map(gif => (
+            <GifView key={gif.id} gif={gif} />
+          ))}
+        </Row>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
