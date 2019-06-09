24d80e47cf52a4def673d87a0762c400dc411a31
Edward Biswas <edbiswas@gmail.com>
2019-06-09 12:44:29 -0400

    Updates

---
+++ precache-manifest.6cc91d6a238ed3a6d810d95a917ac5e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b04186e09a45a505eec455f7bac7f558",
+    "url": "/giphy/index.html"
+  },
+  {
+    "revision": "87c7afba3a908f830cb4",
+    "url": "/giphy/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "5fa5c9af567363503c2c",
+    "url": "/giphy/static/css/main.7816bf99.chunk.css"
+  },
+  {
+    "revision": "87c7afba3a908f830cb4",
+    "url": "/giphy/static/js/2.94199a2d.chunk.js"
+  },
+  {
+    "revision": "5fa5c9af567363503c2c",
+    "url": "/giphy/static/js/main.06db94b1.chunk.js"
+  },
+  {
+    "revision": "2be08ba7eaf0823a821e",
+    "url": "/giphy/static/js/runtime~main.f83d3e2c.js"
+  }
+]);
